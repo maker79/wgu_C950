3c79b4943dc05323bd809fbc244ff13dac48cf59
maker79 <vladan.djurkovic@gmail.com>
2020-08-05 11:57:51 -0700

    repaired hash table and eliminated csv_reader.py file

--- utilities/hashtable.py
+++ utilities/hashtable.py
@@ -1,4 +1,4 @@
-# from utilities.package_csv_reader import get_package_info
+import csv
 
 
 # The goal of this hash table is to improve the speed of accessing the packages
@@ -45,4 +45,22 @@ class PackageHashTable:
                 bucket_list.remove(key)
 
 
+# This method will read the csv file that contains package info
+# and import the data from the package_file.csv to the hashtable
+# Complexity O(N)
+def get_package_info(file_name):
+    packages_list = PackageHashTable()
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        # the next will skip over headers and start reading from 1st actual package, line 2
+        next(csv_reader)
 
+        for line in csv_reader:
+            packages_list.add_package(int(line[0]), line)  # adding package Id as an integer
+
+        return packages_list
+
+
+list_of_packages = get_package_info(
+    u'C:\\Users\\Vladan\\PycharmProjects\\TSP_data_structures_and_algorithms_II\\data\\package_file.csv')
+print(list_of_packages.table)
